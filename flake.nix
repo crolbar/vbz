e421bb1525c6b738aa3526fc04c93488a1250fa9
crolbar <crolbar@crolbar.com>
2025-04-15 21:14:28 +0300

    flake: add package output

--- flake.nix
+++ flake.nix
@@ -4,19 +4,40 @@
   outputs = inputs: let
     system = "x86_64-linux";
     pkgs = import inputs.nixpkgs {inherit system;};
+
+    deps = with pkgs; [
+      miniaudio
+      gcc
+    ];
   in {
     devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        miniaudio
-        gcc
-        clang-tools
-      ];
+      packages = with pkgs;
+        [
+          clang-tools
+        ]
+        ++ deps;
 
-      shellHook = ''
-        export MINIAUDIO="${pkgs.miniaudio}"
+      LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [libpulseaudio];
+    };
+
+    packages.${system}.default = pkgs.buildGoModule {
+      pname = "vbz";
+      version = "0.1";
+      src = ./.;
+      buildInputs = with pkgs; [makeWrapper] ++ deps;
+      installPhase = ''
+        mkdir -p $out/bin
+
+        dir="$GOPATH/bin"
+        [ -e "$dir" ] && cp -r $dir $out
+
+        mkdir -p $out/share/applications
+        cp vbz.desktop $out/share/applications
       '';
-
-      LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [alsa-lib libpulseaudio];
+      postFixup = ''
+        wrapProgram $out/bin/vbz --set LD_LIBRARY_PATH ${pkgs.libpulseaudio}/lib
+      '';
+      vendorHash = "sha256-xKAAPSz1IE8VVR6sNViUicG5Wz5m0IiOkutbOPhywKQ=";
     };
   };
 }
