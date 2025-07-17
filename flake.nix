3ec29e8e823a2770eda99a77729e7837f1619fcc
crolbar <crolbar@crolbar.com>
2025-07-17 11:05:56 +0300

    flake: aarch64-linux build

--- flake.nix
+++ flake.nix
@@ -2,39 +2,45 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = inputs: let
-    system = "x86_64-linux";
-    pkgs = import inputs.nixpkgs {inherit system;};
-
-    deps = with pkgs; [miniaudio];
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
+    pkgsFor = inputs.nixpkgs.legacyPackages;
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs;
-        [
+    devShells = forEachSystem (system: let
+      pkgs = pkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
           clang-tools
-        ]
-        ++ deps;
+          miniaudio
+        ];
 
-      LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [libpulseaudio];
-    };
+        LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [libpulseaudio];
+      };
+    });
 
-    packages.${system}.default = pkgs.buildGoModule {
-      pname = "vbz";
-      version = "0.1";
-      src = ./.;
-      buildInputs = with pkgs; [makeWrapper] ++ deps;
-      installPhase = ''
-        mkdir -p $out/bin
+    packages = forEachSystem (system: let
+      pkgs = pkgsFor.${system};
+    in {
+      default = pkgs.buildGoModule {
+        pname = "vbz";
+        version = "0.1";
+        src = ./.;
+        buildInputs = with pkgs; [makeWrapper miniaudio];
+        installPhase = ''
+          mkdir -p $out/bin
 
-        dir="$GOPATH/bin"
-        [ -e "$dir" ] && cp -r $dir $out
+          dir="$GOPATH/bin"
+          [ -e "$dir" ] && cp -r $dir $out
 
-        mkdir -p $out/share/applications
-        cp vbz.desktop $out/share/applications
-      '';
-      postFixup = ''
-        wrapProgram $out/bin/vbz --set LD_LIBRARY_PATH ${pkgs.libpulseaudio}/lib
-      '';
-      vendorHash = "sha256-xKAAPSz1IE8VVR6sNViUicG5Wz5m0IiOkutbOPhywKQ=";
-    };
+          mkdir -p $out/share/applications
+          cp vbz.desktop $out/share/applications
+        '';
+        postFixup = ''
+          wrapProgram $out/bin/vbz --set LD_LIBRARY_PATH ${pkgs.libpulseaudio}/lib
+        '';
+        vendorHash = "sha256-xKAAPSz1IE8VVR6sNViUicG5Wz5m0IiOkutbOPhywKQ=";
+      };
+    });
   };
 }
